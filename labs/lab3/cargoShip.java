bfab7738647338272600119de5b14aa846464a7d
Rohit <rohit.truesdale@gmail.com>
2023-09-13 22:00:33 -0500

    separated classes into files

---
+++ labs/lab3/cargoShip.java
@@ -0,0 +1,17 @@
+// cargo ship variant class that extends ship class
+class CargoShip extends ship {
+    private int cargoCapacity;
+
+    public CargoShip(String name, String yearBuilt, int cargoCapacity) {
+        super(name, yearBuilt);
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+}
---
+++ labs/lab3/cruiseShip.java
@@ -0,0 +1,18 @@
+// cruise ship class that extends the ship class
+class CruiseShip extends ship {
+    private int maxPassengers;
+
+    public CruiseShip(String name, String yearBuilt, int maxPassengers) {
+        super(name, yearBuilt);
+        this.maxPassengers = maxPassengers;
+    }
+
+    public int getMaxPassengers() {
+        return maxPassengers;
+    }
+
+    public void setMaxPassengers(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+    }
+}
+
