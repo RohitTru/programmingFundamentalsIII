63ca19ab92485c50e446c9922b4e3b0a1dfd5e6a
Rohit Truesdale <rohit.trues@gmail.com>
2024-02-10 17:59:32 -0600

    created seperate main and file with no main and finished main and employee methods

--- labs/lab1/employee.java
+++ labs/lab1/employee.java
@@ -26,8 +26,47 @@ class Employee {
         }
     } 
 
-
-
+    // The function to display stored employee data
+    public void displayEmpData(){
+        JOptionPane.showMessageDialog(null, "Employee Name: " + empName + ",Employee ID: " + empID + ", Gross Salary: $" + grossSalary);
+        JOptionPane.showMessageDialog(null,"Monthly Salary: $" + (grossSalary / 12));
 
     }
 
+    // Gross salary + annual bonus
+    public void showGrossPayOut(){
+        JOptionPane.showMessageDialog(null, "Gross salary: $" + grossSalary + ", Annual Bonus: $" + annualBonus);
+        
+    }
+
+
+
+}
+
+public class Main {
+    public static void main(String[] args){
+        try {
+            for (int i = 0; i < 5; i++){ // prompts user 5 times
+                String empName = JOptionPane.showInputDialog("Please enter the Employee's Name: ");
+                int empID = Integer.parseInt(JOptionPane.showInputDialog("Enter Employee ID: "));
+                int grossSalary = Integer.parseInt(JOptionPane.showInputDialog("Enter gross Salary: " ));
+                double annualBonus = Double.parseDouble(JOptionPane.showInputDialog("Enter annual bonus:"));
+
+                // Creates the employee objects and returns data
+                Employee emp = new Employee(empName, empID, grossSalary, annualBonus);
+                JOptionPane.showMessageDialog(null, "Employee " + (i + 1) + " created: ");
+                emp.displayEmpData();
+                emp.showGrossPayOut();
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null,"Please enter a valid digit format.");
+        }
+
+        } catch (faultyComputationError e){
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+    }
+}
+            
+            
+    
